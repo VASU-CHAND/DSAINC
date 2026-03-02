b9684a222f20a7d387dbc12f2c4eecf81fb5cc1a
VASU CHAND <vasuchand9@gmail.com>
2026-03-02 14:15:00 +0530

    ADD

--- experiments/sixth.c
+++ experiments/sixth.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 int main()
-{
+{ printf("Enter the number of elements: ");
     int n;
     scanf("%d", &n);
     int arr[n];
-
+   printf("Enter the elements: ");
     for (int i = 0; i < n; i++)
         scanf("%d", &arr[i]);
-
+  printf("Enter the element to search: ");
     int key, found = 0;
     scanf("%d", &key);
 
--- experiments/third.c
+++ experiments/third.c
@@ -4,6 +4,7 @@
 int main()
 {
     char str[100];
+    printf("Enter a string: ");
     fgets(str, sizeof(str), stdin);
 
     for (int i = 0; str[i] != '\0'; i++)
